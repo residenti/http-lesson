9563ccf5aef882e04dcd1c96dec231fe45e7ff06
residenti <n.queue.r@gmail.com>
2022-06-02 21:23:00 +0900

    Define variable for creating http request

--- app/Main.hs
+++ app/Main.hs
@@ -6,5 +6,15 @@ import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as LC
 import Network.HTTP.Simple
 
+-- NOTE: NOAA Climate Data APIのデータセットを取得するためのHTTPリクエスト
+myToken :: BC.ByteString
+myToken = "<APIトークン>"
+
+noaaHost :: BC.ByteString
+noaaHost = "www.ncdc.noaa.gov"
+
+apiPath :: BC.ByteString
+apiPath = "/cdo-Web/api/v2/datasets"
+
 main :: IO ()
 main = print "hi"
